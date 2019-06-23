b091035245fb8d909ba49c283a95adc8603c5f73
Santosh Marigowda <santosharakere@gmail.com>
2019-06-23 10:26:38 +0100

    adding jspdf and pdfjslib

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import PDFMasker from './modules/PDFMasker';
+
+const pdfMasker = new PDFMasker('A Module to abstract PDF Masking....');
+pdfMasker.printTitle();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/modules/PDFMasker.js
@@ -0,0 +1,8 @@
+export default class PDFMasker {
+    constructor(title) {
+        this.title = title;
+    }
+    printTitle() {
+        console.log(this.title);
+    }
+}
